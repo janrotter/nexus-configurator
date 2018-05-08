399386da7be87b50e4214fa34bf1f1ba4316994e
Jan Rotter <jan.rotter@ocado.com>
2018-05-08 10:56:01 +0200

    Add support for s3:// and http:// schemas for the config and pass files.

--- tests/test_nexus_configurator.py
+++ tests/test_nexus_configurator.py
@@ -1,9 +1,18 @@
+import base64
 import sys
 from unittest import mock
 from unittest.mock import patch
-sys.path.insert(0, 'bin')
-import nexus_configurator
+
+import boto3
+import httpretty
+from moto import mock_s3
+
 from nexus.nexus import NexusUnauthorised
+sys.path.insert(0, 'bin')
+import nexus_configurator # noqa: E402
+
+SAMPLE_CONFIG = [{'setup_anonymous_access': [{'anonymous_access': True}]}]
+SAMPLE_CONFIG_PATH = 'tests/test_data/example_config.yaml'
 
 
 @mock.patch('nexus_configurator.Nexus')
@@ -11,7 +20,7 @@ def test_nexus_sets_up_anonymous_access(nexus):
     anon_access_script = 'groovy/setup_anonymous_access.groovy'
     testargs = ["nexus_configurator",
                 "--config",
-                "tests/test_data/example_config.yaml"]
+                SAMPLE_CONFIG_PATH]
     with patch.object(sys, 'argv', testargs):
         nexus_configurator.main()
 
@@ -24,7 +33,7 @@ def test_nexus_sets_up_anonymous_access(nexus):
 def test_nexus_auth_tries_each_password_until_successful(nexus):
     testargs = ["nexus_configurator",
                 "--config",
-                "tests/test_data/example_config.yaml",
+                SAMPLE_CONFIG_PATH,
                 "-c",
                 "tests/test_data/test_auth"]
     passwords = ['admin123', 'admin12345', 'pass2', 'pass3']
@@ -38,3 +47,42 @@ def test_nexus_auth_tries_each_password_until_successful(nexus):
         nexus.assert_any_call(host='http://localhost:8081',
                               password=p,
                               user='admin')
+
+
+@mock_s3
+def test_read_config_file_from_s3():
+    bucket_name = 'config_bucket'
+    s3_key = 'configfilename'
+
+    s3_client = boto3.client('s3')
+    s3_client.create_bucket(Bucket=bucket_name)
+    with open(SAMPLE_CONFIG_PATH) as f:
+        body = f.read()
+
+    s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=body)
+
+    s3_url = 's3://' + bucket_name + '/' + s3_key
+    config = nexus_configurator.read_config_file(s3_url)
+    assert(config == SAMPLE_CONFIG)
+
+
+def test_read_config_file_from_disk():
+    config_file_path = SAMPLE_CONFIG_PATH
+    config = nexus_configurator.read_config_file(config_file_path)
+    assert(config == SAMPLE_CONFIG)
+
+
+@httpretty.activate
+def test_read_config_file_from_http_with_auth():
+    config_url = 'http://someuser:somepass@configserver.bar/config'
+    with open(SAMPLE_CONFIG_PATH) as f:
+        body = f.read()
+    httpretty.register_uri(
+        httpretty.GET,
+        config_url,
+        body=body
+    )
+    config = nexus_configurator.read_config_file(config_url)
+    assert(httpretty.last_request().headers['Authorization'] ==
+           'Basic ' + base64.b64encode(b'someuser:somepass').decode())
+    assert(config == SAMPLE_CONFIG)
